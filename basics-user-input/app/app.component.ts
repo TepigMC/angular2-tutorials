28240e51693e2e7cffd80c1b089696d926f0891b
Andrew Archibald <andrew@robarchibald.com>
2016-05-21 15:44:26 -0700

    moved template to html file

--- basics-user-input/app/app.component.ts
+++ basics-user-input/app/app.component.ts
@@ -7,23 +7,7 @@ import { LoopBackComponent } from './loop-back.component';
 
 @Component({
   selector: 'my-app',
-  template: `
-    <h1>User Input</h1>
-    <h2>Bind to click</h2>
-    <click-me></click-me>
-    <h2>Bind to keyup</h2>
-    <h3>Get user input from the $event object</h3>
-    <key-up1></key-up1>
-    <h3>Get user input from a template reference variable</h3>
-    <loop-back></loop-back>
-    <key-up2></key-up2>
-    <h3>Key event filtering (with key.enter)</h3>
-    <key-up3></key-up3>
-    <h3>On blur</h3>
-    <key-up4></key-up4>
-    <h2>Little Tour of Heroes</h2>
-    <little-tour></little-tour>
-  `,
+  templateUrl: 'app.component.html',
   directives: [
     ClickMeComponent, KeyUpComponent_v1, KeyUpComponent_v2, KeyUpComponent_v3,
     KeyUpComponent_v4, LittleTourComponent, LoopBackComponent
